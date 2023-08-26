7b21512b57b12e53ae5ed22ea614f62131688a49
Luis Weich <luis.weich.de@gmail.com>
2023-08-26 13:34:53 +0200

    index.js can forward emails to gotify

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+import "dotenv/config"
 import MailDev from "maildev"
+import Gotify from "./gotify.js"
 
-const SMTP_PORT = process.env.SMTP_PORT || 1025
+const EMAIL_SMTP_PORT = process.env.EMAIL_SMTP_PORT || 1025
+
+const GOTIFY_URL = process.env.GOTIFY_URL
+const GOTIFY_TOKEN = process.env.GOTIFY_TOKEN
 
 const maildev = new MailDev({
-    smtp: SMTP_PORT,
+    smtp: EMAIL_SMTP_PORT,
     disableWeb: true
 })
 
-maildev.on("new", (email) => {
-    console.log("new email", email)
+const gotify = new Gotify(GOTIFY_URL, GOTIFY_TOKEN)
+
+maildev.on("new", async (email) => {
+    try {
+        await gotify.send(email.subject, email.text)
+    } catch (e) {
+        console.error("failed to forward email", e.message)
+    }
 })
+
+maildev.listen()

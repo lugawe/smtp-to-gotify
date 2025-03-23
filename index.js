b4dc60540908d81e789448489795aa4abaa2fef5
Luis Weich <luis.weich.de@gmail.com>
2025-03-23 17:59:36 +0100

    update and migrate to bun

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import "dotenv/config"
 import MailDev from "maildev"
 import Gotify from "./gotify.js"
 
@@ -6,6 +5,14 @@ const EMAIL_SMTP_PORT = process.env.EMAIL_SMTP_PORT || 1025
 
 const GOTIFY_URL = process.env.GOTIFY_URL
 const GOTIFY_TOKEN = process.env.GOTIFY_TOKEN
+
+if (!GOTIFY_URL) {
+    throw new Error("GOTIFY_URL cannot be empty")
+}
+
+if (!GOTIFY_TOKEN) {
+    throw new Error("GOTIFY_TOKEN cannot be empty")
+}
 
 const maildev = new MailDev({
     smtp: EMAIL_SMTP_PORT,
